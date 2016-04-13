77a31ec2fcd63bf357b019e43bc23ca438428c06
Darren Carter <darren@tremendusapps.com>
2016-04-13 12:18:29 -0700

    Updated JS for login

--- js/codeclub-home.js
+++ js/codeclub-home.js
@@ -1,7 +1,25 @@
 var fb = new Firebase('https://tremendus-ember-app.firebaseio.com/codeclub')
+
 var data = {
   users: fb.child('users'),
   rooms: fb.child('rooms')
+}
+
+var session = window.session = {
+  user: {}
+}
+
+fb.onAuth(function (user) {
+  console.log('auth():session', session)
+  if (user) {
+    session.user = user
+    save(user)
+  }
+})
+
+function save (user) {
+  console.log('save()', user)
+  data.users.child(user.uid).set(user)
 }
 
 var vue = new Vue({
@@ -9,7 +27,7 @@ var vue = new Vue({
   el: "#app",
   data: function () {
     return {
-      msg: 'hello world',
+      session: window.session,
       user: {
         alias: ''
       },
@@ -31,8 +49,19 @@ var vue = new Vue({
   methods: {
     save: function (model) {
       console.log('model', model)
-      data[model+'s'].push(this.$get(model))
+      var _model = this.$get(model)
+      _model.addedBy = this.session.user.google.displayName
+      data[model+'s'].push(_model)
       this[model] = { alias: null, name: null }
+    },
+    login: function () {
+      fb.authWithOAuthPopup('google', function (user) {
+        console.log('loggedIn', user)
+      }, { scope: 'email' })
+    },
+    logout: function () {
+      fb.unauth()
+      window.session.user = {}
     }
   }
 })
